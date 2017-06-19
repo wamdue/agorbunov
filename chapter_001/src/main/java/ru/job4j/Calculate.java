882610c5e3d442ef17c9cc6d9974be53e5c10080
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-06-19 15:42:24 +0300

    add checkstyle support

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -4,13 +4,12 @@ package ru.job4j;
     Класс точки входа, с выводом на экран.
 */
 
-public class Calculate{
-
+public class Calculate {
     /**
 	Вывод строки на экран.
+	@param args params
     */
-    public static void main(String[] args){
+    public static void main(String[] args) {
 	System.out.println("Hello World");
     }
-
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexey Gorbunov (mailto:wamdue.m@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
