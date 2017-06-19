2277488d79fa383909e287b86a6b76e77df5d02f
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-06-19 13:51:21 +0300

    add comments

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
 
+/**
+    Класс точки входа, с выводом на экран.
+*/
+
 public class Calculate{
+
+    /**
+	Вывод строки на экран.
+    */
     public static void main(String[] args){
 	System.out.println("Hello World!");
     }
