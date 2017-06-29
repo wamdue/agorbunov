000c2e6a5cf7eaa7bce01b3fc7ad280ee532381f
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-06-29 10:55:07 +0300

    make changes in class Board.java, task [#17526]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -13,12 +13,10 @@ public class Board {
 	    StringBuilder sb = new StringBuilder();
 	    for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
-		    if (y == 0 || y % 2 == 0) {
-			if (y + x % 2 == 0) {
-			    sb.append("X");
-			} else {
-			    sb.append(" ");
-			}
+		    if ((y + x) % 2 == 0) {
+			sb.append("X");
+		    } else {
+		        sb.append(" ");
 		    }
 		}
 		sb.append("\n");
