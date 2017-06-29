98782f131ef3dd9570a128372c8a010be9c953dc
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-06-29 10:53:52 +0300

    make changes in class Triangle.java task #17483

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -30,9 +30,12 @@ public class Triangle {
     * @return area of triangle if possible, or -1 if it cannot be.
     */
     public double area() {
-	if ((a.getX() == b.getX() && a.getX() == c.getX())
+	if ((a.getX() == b.getY() && a.getY() == b.getY())
+	|| (b.getX() == c.getX() && b.getY() == c.getY())
+	|| (a.getX() == c.getX() && a.getY() == c.getY())
+	|| (a.getX() == b.getX() && a.getX() == c.getX())
 	|| (a.getY() == b.getY() && a.getY() == c.getY())) {
-	    return -1;
+	    return -1D;
 	}
 	return Math.abs((Double.valueOf((a.getX() - b.getX()) * (c.getY() - b.getY()))
 		- Double.valueOf((a.getY() - b.getY()) * (c.getX() - b.getX()))) / 2);
