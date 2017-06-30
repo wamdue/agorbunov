0e060bb38cfd7f218024306a15aa7e30c38e3c97
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-06-30 09:08:55 +0300

    add solution for task #396

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,7 +7,7 @@ public class Tracker {
     /**
     * @param items - array of tasks.
     */
-    private Item[] items = new item[100];
+    private Item[] items = new Item[100];
     /**
     * @param position - id of last element.
     */
@@ -16,7 +16,10 @@ public class Tracker {
     * @param item - item to add and generate id for it.
     */
     public Item add(Item item) {
-	this.items[position++] = item;
+	if(position == 100) {
+	    position--;
+	}
+	items[position++] = item;
 	return item;
     }
     /**
@@ -36,24 +39,30 @@ public class Tracker {
     public void delete(Item item) {
 	for (int i = 0; i < position; i++) {
 	    if(items[i].getId().equals(item.getId())) {
-		System.arrayCopy(items, i + 1, items, i, position--);
+		System.arraycopy(items, i + 1, items, i, position--);
 		break;
 	    }
 	}
     }
     public Item[] findAll() {
-	return this.items;
+	return Arrays.copyOf(items, position);
     }
     /**
-    *
+    * @param key - value to search in items.
+    * @return array - array of finded values.
     */
     public Item[] findByName(String key) {
+	Item[] array = new Item[1];
 	int counter = 0;
 	for (int i = 0; i < position; i++) {
 	    if (items[i].getName().contains(key)) {
-		counter++;
+		if (counter == array.length) {
+		    array = Arrays.copyOf(array, counter + 1);
+		}
+		array[counter++] = items[i];
 	    }
 	}
+	return array[0] == null ? null : array;
     }
     /**
     * @param id - id the task, which you want to find.
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,108 @@
+package ru.job4j.tracker;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+/**
+* Class for testing Tracker.
+*/
+public class TrackerTest {
+    /**
+    * Adding 5 elements, and expection 5 elements in array.
+    */
+    @Test
+    public void whenAddNewItemThenIncrase() {
+	Tracker tracker = new Tracker();
+	for (int i = 0; i < 5; i++) {
+	    tracker.add(new Item(Integer.toString(i), Integer.toString(i)
+		, null));
+	}
+	int expected = 5;
+	assertThat(tracker.size(), is(expected));
+    }
+    /**
+    * Adding 5 elements, then remove 1, end expecting 4.
+    */
+    @Test
+    public void whenRemoveOneThenLess() {
+	Tracker tracker = new Tracker();
+	Item[] items = new Item[5];
+	Item item = null;
+	for (int i = 0; i < 5; i++) {
+	    item = new Item(Integer.toString(i), Integer.toString(i), null);
+	    item.setId(Integer.toString(i));
+	    items[i] = item;
+	    tracker.add(item);
+	}
+	tracker.delete(items[1]);
+	int expected = 4;
+	assertThat(tracker.size(), is(expected));
+    }
+    /**
+    * Compare two arrays.
+    */
+    @Test
+    public void whenHaveTwoEqualArraysThenEqual() {
+	Item[] items = new Item[5];
+	Tracker tracker = new Tracker();
+	Item item = null;
+	for (int i = 0; i < 5; i++) {
+	    item = new Item(Integer.toString(i), Integer.toString(i), null);
+	    item.setId(Integer.toString(i));
+	    items[i] = item;
+	    tracker.add(item);
+	}
+	assertThat(tracker.findAll(), is(items));
+    }
+    /**
+    * Find item by name.
+    */
+    @Test
+    public void whenHaveNameThenReturnByName() {
+	Tracker tracker = new Tracker();
+	Item[] items = new Item[5];
+	Item item = null;
+	for (int i = 0; i < 5; i++) {
+	    item = new Item(Integer.toString(i), Integer.toString(i), null);
+	    item.setId(Integer.toString(i));
+	    items[i] = item;
+	    tracker.add(item);
+	}
+	Item[] expected = new Item[1];
+	expected[0] = items[2];
+	assertThat(tracker.findByName("2"), is(expected));
+    }
+    /**
+    * Find item by id.
+    */
+    @Test
+    public void whenHaveIdThenReturnById() {
+	Tracker tracker = new Tracker();
+	Item[] items = new Item[5];
+	Item item = null;
+	for (int i = 0; i < 5; i++) {
+	    item = new Item(Integer.toString(i), Integer.toString(i), null);
+	    item.setId(Integer.toString(i));
+	    items[i] = item;
+	    tracker.add(item);
+	}
+	assertThat(tracker.findById("1"), is(items[1]));
+    }
+    /**
+    * Update item.
+    */
+    @Test
+    public void whenNeedToUpdateThenUpdate() {
+	Tracker tracker = new Tracker();
+	Item[] items = new Item[5];
+	Item item = null;
+	for (int i = 0; i < 5; i++) {
+	    item = new Item(Integer.toString(i), Integer.toString(i), null);
+	    item.setId(Integer.toString(i));
+	    items[i] = item;
+	    tracker.add(item);
+	}
+	items[1].setName("TestName");
+	tracker.update(items[1]);
+	assertThat(tracker.findAll(), is(items));
+    }
+}
