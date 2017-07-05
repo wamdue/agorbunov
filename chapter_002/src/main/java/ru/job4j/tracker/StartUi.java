4e0692c2974ae2c187f3c2b24a38b1bdeaa4f8d8
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-07-05 10:28:59 +0300

    refactor StartUi class task #784

--- chapter_002/src/main/java/ru/job4j/tracker/StartUi.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUi.java
@@ -4,6 +4,34 @@ package ru.job4j.tracker;
 */
 public class StartUi {
     /**
+    * @param ADD - add new record.
+    */
+    private static final String ADD = "0";
+    /**
+    * @param SHOW_ALL - view all records.
+    */
+    private static final String SHOW_ALL = "1";
+    /**
+    * @param EDIT - edit record by id.
+    */
+    private static final String EDIT = "2";
+    /**
+    * @param DELETE - delete record by id.
+    */
+    private static final String DELETE = "3";
+    /**
+    * @param FIND_BY_ID - find record by id.
+    */
+    private static final String FIND_BY_ID = "4";
+    /**
+    * @param FIND_BY_NAME - find records by name.
+    */
+    private static final String FIND_BY_NAME = "5";
+    /**
+    * @param EXIT - exit program.
+    */
+    private static final String EXIT = "6";
+    /**
     * @param menu - main menu.
     */
     private String[] menu = {"0 Add new Item",
@@ -16,7 +44,7 @@ public class StartUi {
     /**
     * @param input - class for drawing main menu, and read input.
     */
-    private ConsoleInput input;
+    private Input input;
     /**
     * @param tracker - class tracker.
     */
@@ -26,48 +54,46 @@ public class StartUi {
     * Entry point!
     */
     public static void main(String[] args) {
-	new StartUi();
+    	new StartUi(new ConsoleInput(), new Tracker()).init();
     }
     /**
-    * Constructor with no args.
+    * Constructor.
     */
-    public StartUi() {
-	this.init();
+    public StartUi(Input input, Tracker tracker) {
+    	this.input = input;
+    	this.tracker = tracker;
     }
     /**
     * Init class for UI.
     */
     public void init() {
-	this.input = new ConsoleInput();
-	this.tracker = new Tracker();
-	int element;
-	while (true) {
-	    this.drawMenu();
-	    try{
-		element = Integer.valueOf(input.ask("select: "));
-		if (element == 0) {
-		    this.addElement();
-		} else if (element == 1) {
-		    this.showElements(tracker.findAll());
-		} else if (element == 2) {
-		    this.editElement();
-		} else if (element == 3) {
-		    this.deleteElement();
-		} else if (element == 4) {
-		    this.elementById();
-		} else if (element == 5) {
-		    this.elementsByName();
-		} else if (element == 6) {
-		    System.out.println("Have a nice day!");
-		    break;
-		} else {
-		    continue;
-		}
-	    } catch (Exception ex) {
-		System.out.println("Must be number from 0 to 6");
-	    }
-	    
-	}
+    	String element;
+    	while (true) {
+    		this.drawMenu();
+    		try{
+    			element = input.ask("select: ");
+    			if (element.equals(ADD)) {
+    				this.addElement();
+    			} else if (element.equals(SHOW_ALL)) {
+    				this.showElements(tracker.findAll());
+    			} else if (element.equals(EDIT)) {
+    				this.editElement();
+    			} else if (element.equals(DELETE)) {
+    				this.deleteElement();
+    			} else if (element.equals(FIND_BY_ID)) {
+    				this.elementById();
+    			} else if (element.equals(FIND_BY_NAME)) {
+    			    this.elementsByName();
+    			} else if (element.equals(EXIT)) {
+			    break;
+    			} else {
+    			    continue;
+    				//System.out.println("Что-то пошло не так!");
+    			}
+    		} catch (Exception ex) {
+    			System.out.println("Must be number from 0 to 6");
+    		}	    
+    	}
     }
     /**
     * Shows main menu.
@@ -82,62 +108,63 @@ public class StartUi {
     * Add new task to tracker.
     */
     public void addElement() {
-	String name = this.input.ask("Enter task name: ");
-	String desc = this.input.ask("Enter description: ");
-	String[] comment = {this.input.ask("Enter comment: ")};
-	tracker.add(new Item(name, desc, comment));
+    	String name = this.input.ask("Enter task name: ");
+    	String desc = this.input.ask("Enter description: ");
+//    	String[] comment = {this.input.ask("Enter comment: ")};
+    	tracker.add(new Item(name, desc));
     }
     /**
     * Prints array of items.
     */
     public void showElements(Item[] items) {
-	if (items != null && items.length > 0) {
-	    for (Item item : items) {
-	        System.out.println("Id: " + item.getId());
-	        System.out.println("Creation time: " + item.getCreated());
-	        System.out.println("Task name: " + item.getName());
-	        System.out.println("Description: " + item.getDesc());
-	        if (item.getComments() != null) {
-    		    System.out.print("Comments: ");
-		    for(String s : item.getComments()) {
-		    System.out.println(s);
-		    }
-		}
-		System.out.println("----------------");
-	    }
-	} else {
-	    System.out.println("Task`s list is empty.");
-	}
+    	if (items != null && items.length > 0) {
+    		for (Item item : items) {
+    			System.out.println("Id: " + item.getId());
+    			System.out.println("Creation time: " + item.getCreated());
+    			System.out.println("Task name: " + item.getName());
+    			System.out.println("Description: " + item.getDesc());
+    			if (item.getComments() != null) {
+    				System.out.print("Comments: ");
+    				for(String s : item.getComments()) {
+    					System.out.println(s);
+    				}
+    			}
+    			System.out.println("----------------");
+    		}
+    	} else {
+    		System.out.println("Task`s list is empty.");
+    	}
     }
     /**
     * Edit item from tracker.
     */
     public void editElement() {
-	Item item = tracker.findById(this.input.ask("Enter task id for edit: "));
-	String name = this.input.ask("Enter task name, or leave empty: ");
-	String desc = this.input.ask("Enter new description or leave empty: ");
-	item.setName(name != "" ? name : item.getName());
-	item.setDesc(desc != "" ? desc : item.getDesc());
-	tracker.update(item);
+    	Item item = tracker.findById(this.input.ask("Enter task id for edit: "));
+		String name = this.input.ask("Enter task name, or leave empty: ");
+		String desc = this.input.ask("Enter new description or leave empty: ");
+		item.setName(name != "" ? name : item.getName());
+		item.setDesc(desc != "" ? desc : item.getDesc());
+		tracker.update(item);
     }
     /**
     * Delete item from tracker.
     */
     public void deleteElement() {
-	String temp = this.input.ask("Enter task id for delete: ");
-	tracker.delete(tracker.findById(temp));
+	String id = this.input.ask("Enter task id for delete: ");
+	System.out.println("id = " + id);
+	tracker.delete(tracker.findById(id));
     }
     /**
     * Show items on console, founded by name.
     */
     public void elementsByName() {
-	this.showElements(tracker.findByName(this.input.ask("Enter task name: ")));
+    	this.showElements(tracker.findByName(this.input.ask("Enter task name: ")));
     }
     /**
     * Show item on console, found by id.
     */
     public void elementById() {
-	Item[] item = {tracker.findById(this.input.ask("Enter task id : "))};
-	this.showElements(item);
+    	Item[] item = {tracker.findById(this.input.ask("Enter task id : "))};
+    	this.showElements(item);
     }
 }
