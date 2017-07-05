d81ed36058bbb3358ecf3e1bca00d38a01e828b5
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-07-05 08:58:34 +0300

    refactor Triangle class task#9461

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -30,20 +30,14 @@ public class Triangle {
     * @return area of triangle if possible, or -1 if it cannot be.
     */
     public double area() {
-	if ((calcSide(a, b) < calcSide(a, c) + calcSide(b, c))
-	 && (calcSide(a, c) < calcSide(a, b) + calcSide(b, c))
-	 && (calcSide(b, c) < calcSide(a, b) + calcSide(a, c))) {
+	double ab = Math.sqrt(Math.sqrt(b.getX() - a.getX()) + Math.sqrt(b.getY() - a.getY()));
+	double bc = Math.sqrt(Math.sqrt(c.getX() - b.getX()) + Math.sqrt(c.getY() - b.getY()));
+	double ac = Math.sqrt(Math.sqrt(c.getX() - a.getX()) + Math.sqrt(c.getY() - a.getY()));
+
+	if ((ab < bc + ac) && (ac < ab + bc) && (bc < ab + ac)) {
 	    return Math.abs((Double.valueOf((a.getX() - b.getX()) * (c.getY() - b.getY()))
 		- Double.valueOf((a.getY() - b.getY()) * (c.getX() - b.getX()))) / 2);
 	}
 	return -1D;
-    }
-    /**
-    * @param a - first point.
-    * @param b - second side.
-    * @return length of a side.
-    */
-    public double calcSide(Point a, Point b) {
-	return Math.sqrt(Math.sqrt(b.getX() - a.getX()) + Math.sqrt(b.getY() - a.getY()));
     }
 }
