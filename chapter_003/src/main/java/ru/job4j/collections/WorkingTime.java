009f56a439dc2bec53823173a83801c58db53ba3
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-07-14 16:12:28 +0300

    adding class for compare collections task #10022

---
+++ chapter_003/src/main/java/ru/job4j/collections/WorkingTime.java
@@ -0,0 +1,69 @@
+package ru.job4j.collections;
+
+import java.util.*;
+
+/**
+ * Created on 14.07.17
+ * Class for calculation working time of the collections.
+ * @author Wamdue
+ * @version 1.0
+ */
+public class WorkingTime {
+    /**
+     * Main constructor
+     */
+    public WorkingTime() {}
+
+    /**
+     * Calculate insert working time.
+     * @param collection - source collection.
+     * @param amount - amount string need to insert.
+     * @return - working time
+     */
+    public long add(Collection<String> collection, int amount) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < amount; i++) {
+            collection.add(String.format("%s %d", collection.getClass().getName(), i));
+        }
+        long stopTime = System.currentTimeMillis();
+        return stopTime - startTime;
+    }
+
+    /**
+     * Calculate delete working time.
+     * @param collection - source collection.
+     * @param amount - how many records need to delete.
+     * @return working time.
+     */
+    public long delete(Collection<String> collection, int amount) {
+        long startTime = System.currentTimeMillis();
+        Iterator<String> iterator = collection.iterator();
+        for (int i = 0; i < amount; i++) {
+            iterator.next();
+            iterator.remove();
+        }
+        long stopTime = System.currentTimeMillis();
+        return stopTime - startTime;
+    }
+
+    /**
+     * Private method for information.
+     * @param collection - source collection for interact.
+     * @param count - number of iterations.
+     */
+    private void getResult(Collection<String> collection, int count) {
+        System.out.println(collection.getClass().getName());
+        System.out.printf("Insert: %d\n", this.add(collection, count));
+        System.out.printf("Delete: %d\n", this.delete(collection, count));
+    }
+
+    public static void main(String[] args) {
+        WorkingTime time = new WorkingTime();
+        ArrayList<String> list = new ArrayList<>();
+        LinkedList<String> linked = new LinkedList<>();
+        TreeSet<String> set = new TreeSet<>();
+        time.getResult(list, 1000);
+        time.getResult(linked, 1000);
+        time.getResult(set, 1000);
+    }
+}
