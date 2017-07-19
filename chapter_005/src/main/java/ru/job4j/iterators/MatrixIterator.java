d5fae8eee2f82ebc548a69e7c64de7156a8b62fb
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-07-19 16:13:17 +0300

    add MatrixIterator class, task #9539

---
+++ chapter_005/src/main/java/ru/job4j/iterators/MatrixIterator.java
@@ -0,0 +1,55 @@
+package ru.job4j.iterators;
+
+import java.util.Iterator;
+
+/**
+ * Created on 19.07.17
+ * Class for iterate matrix.
+ * @author Wamdue
+ * @version 1.0
+ */
+public class MatrixIterator implements Iterator {
+    /**
+     *  Main matrix for iterate.
+     */
+    private Integer[][] matrix;
+    /**
+     * Column number.
+     */
+    private int col = 0;
+    /**
+     * Row number.
+     */
+    private int row = 0;
+
+    /**
+     * Main constructor.
+     * @param matrix - source matrix.
+     */
+    public MatrixIterator(Integer[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    /**
+     * Check, if can move forward.
+     * @return true if can, or false if it can.
+     */
+    @Override
+    public boolean hasNext() {
+        return col < matrix.length;
+    }
+
+    /**
+     * Return number, and move bracket to the next position.
+     * @return matrix cell.
+     */
+    @Override
+    public Integer next() {
+        if (col >= matrix[row].length) {
+            col = 0;
+            row++;
+        }
+        Integer value = matrix[row][col++];
+        return value;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/iterators/MatrixIteratorTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.iterators;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+/**
+ * Created on 19.07.17
+ *
+ * @author Wamdue
+ * @version 1.0
+ */
+public class MatrixIteratorTest {
+    /**
+     * Test hasNext().
+     * Have matrix {0, 1}
+     *             {2, 3}
+     * Call next() 4 times.
+     * call hasNext() expect: false.
+     */
+    @Test
+    public void whenMatrixEndsThenReturnFalse() {
+        boolean expect = false;
+        Integer[][] matrix = new Integer[][] {
+                {0, 1},
+                {2, 3}
+        };
+        MatrixIterator iterator = new MatrixIterator(matrix);
+
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        assertThat(iterator.hasNext(), is(expect));
+    }
+
+    /**
+     * Test next().
+     *  Have matrix  {0, 1, 2},
+     *               {3, 4, 5},
+     *               {6, 7, 8}
+     * 4 times call next.
+     * expect value = 3.
+     */
+    @Test
+    public void whenHaveMoreThenOneColumnThenIterateNext() {
+        Integer[][] matrix = new Integer[][] {
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8}
+        };
+        MatrixIterator iterator = new MatrixIterator(matrix);
+        int expect = 3;
+
+        iterator.next();
+        iterator.next();
+        iterator.next();
+
+        assertThat(iterator.next(), is(expect));
+    }
+
+}
