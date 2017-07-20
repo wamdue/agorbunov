f046206f8c709b3b6f627927d16b8a1449a4e172
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-07-20 11:55:07 +0300

    add class PrimeIt, task #151

---
+++ chapter_005/src/main/java/ru/job4j/iterators/PrimeIt.java
@@ -0,0 +1,83 @@
+package ru.job4j.iterators;
+
+import java.util.Iterator;
+
+/**
+ * Created on 20.07.17
+ *
+ * @author Wamdue
+ * @version 1.0
+ */
+public class PrimeIt implements Iterator {
+    /**
+     * Source array.
+     */
+    private int[] numbers;
+    /**
+     * Current bracket position.
+     */
+    private int position = 0;
+    /**
+     * Main constructor.
+     * @param numbers - source array.
+     */
+    public PrimeIt(final int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    /**
+     *
+     * @return true if prime number exist next from bracket.
+     */
+    @Override
+    public boolean hasNext() {
+        return checkNumber() != -1;
+    }
+
+    /**
+     *
+     * @return prime number.
+     * @throws IndexOutOfBoundsException if prime number cannot be found.
+     */
+    @Override
+    public Object next() {
+        int result = checkNumber();
+        if (checkNumber() > -1) {
+            position = result + 1;
+            return numbers[result];
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    /**
+     * Internal method to check array.
+     * @return index of prime number in array or -1 if it cannot be found.
+     */
+    private int checkNumber() {
+        int result = -1;
+        if (position < numbers.length) {
+            for (int i = position; i < numbers.length; i++) {
+                if (numbers[i] == 1) {
+                    result = i;
+                    break;
+                } else {
+                    int counter = 2;
+
+                    while (counter <= numbers[i]) {
+                        if (numbers[i] % counter == 0) {
+                            break;
+                        }
+                        counter++;
+                    }
+
+                    if (counter == numbers[i]) {
+                        result = i;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/iterators/PrimeItTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.iterators;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created on 20.07.17
+ *
+ * @author Wamdue
+ * @version 1.0
+ */
+public class PrimeItTest {
+    /**
+     * Testing if not have any prime numbers.
+     * have array: 1, 4, 6, 8, 10.
+     * call  next() then hasNext().
+     * expect false;
+     */
+    @Test
+    public void whenDoNotHaveMorePrimeNumbersThenReturnFalse() {
+        int[] primes = new int[] {1, 4, 6, 8, 10};
+        PrimeIt evenIt = new PrimeIt(primes);
+        boolean expect = false;
+        evenIt.next();
+        assertThat(evenIt.hasNext(), is(expect));
+    }
+
+    /**
+     * Testing return next prime value.
+     * have array: 4, 6, 7, 5.
+     * call next().
+     * Expect: 5.
+     */
+    @Test
+    public void whenHavePrimesInArrayThenReturn() {
+        int[] primes = new int[] {4, 6, 7, 5};
+        PrimeIt primeIt = new PrimeIt(primes);
+        int expect = 5;
+        primeIt.next();
+        assertThat(primeIt.next(), is(expect));
+    }
+
+    /**
+     * Testing if have any prime numbers.
+     * have array: 4, 1, 1, 6.
+     * call next() then hasNext()
+     * expect true;
+     */
+    @Test
+    public void whenHaveMoreEvenNumbersThenReturnTrue() {
+        int[] primes = new int[] {4, 1, 1, 6};
+        PrimeIt primeIt = new PrimeIt(primes);
+        boolean expect = true;
+        primeIt.next();
+        assertThat(primeIt.hasNext(), is(expect));
+    }
+}
