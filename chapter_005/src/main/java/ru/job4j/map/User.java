1946869767bc8d0f74a9f7062ae04ab9e1d69d62
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-07-28 12:41:41 +0300

    refactor User class, add equals realization, task #1004

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -89,10 +89,14 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
-        result = 31 * result + children;
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (children != user.children) return false;
+        if (!name.equals(user.name)) return false;
+        return birthday.equals(user.birthday);
     }
 }
