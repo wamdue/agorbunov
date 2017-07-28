6b3ca57ecf568e9d02d29148956da15047753883
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-07-28 12:01:22 +0300

    add User class, task #999.

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,90 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Created on 28.07.17
+ * User class.
+ * @author Wamdue
+ * @version 1.0
+ */
+public class User {
+    /**
+     * User name.
+     */
+    private String name;
+    /**
+     * Date of birthday.
+     */
+    private Calendar birthday;
+    /**
+     * Number of children.
+     */
+    private int children;
+
+    /**
+     * Empty constructor.
+     */
+    public User() {
+    }
+
+    /**
+     * Main constructor.
+     * @param name - user name.
+     * @param birthday - birthday date.
+     * @param children - number of children.
+     */
+    public User(String name, Calendar birthday, int children) {
+        this.name = name;
+        this.birthday = birthday;
+        this.children = children;
+    }
+
+    /**
+     *
+     * @return user name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name - new user name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return date of birthday.
+     */
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    /**
+     *
+     * @param birthday set correct date of birthday.
+     */
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     *
+     * @return current number of children.
+     */
+    public int getChildren() {
+        return children;
+    }
+
+    /**
+     *
+     * @param children = new number of children.
+     */
+    public void setChildren(int children) {
+        this.children = children;
+    }
+}
