1ff29c417c6ab005a1f8f1dd71012386c379e498
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-09-13 12:52:03 +0300

    ParallelSearch class, task #1106

---
+++ chapter_007/src/main/java/ru/job4j/monitor/ParallelSearch.java
@@ -0,0 +1,142 @@
+package ru.job4j.monitor;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created on 12.09.17
+ * Thread safe parallel file search.
+ * @author Wamdue
+ * @version 1.0
+ */
+@ThreadSafe
+public class ParallelSearch {
+
+    public final static String STOP_WORD = "STOP";
+    private final Object lock = new Object();
+    @GuardedBy("lock")
+    private Queue<String> queue = new LinkedList<>();
+
+    private List<String> result = new ArrayList<>();
+
+    private String root;
+    private String text;
+    private List<String> exts;
+
+    /**
+     * Main contructor.
+     * @param root - directory to start search.
+     * @param text - text to search in files.
+     * @param exts - list of extensions to analise.
+     */
+    public ParallelSearch(String root, String text, List<String> exts) {
+        this.root = root;
+        this.text = text;
+        this.exts = exts;
+    }
+
+    /**
+     * Starts to thread to filter files.
+     * @return list of corrected files.
+     */
+    public List<String> result() {
+        PickFile files = new PickFile();
+        Visitor filter = new Visitor();
+        Thread fileFilter = new Thread(files);
+        Thread textSearch = new Thread(filter);
+        fileFilter.start();
+        textSearch.start();
+        try {
+            while (fileFilter.isAlive() && textSearch.isAlive()) {
+                Thread.sleep(100);
+            }
+        } catch(InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Class to filter files by extension.
+     */
+    private class PickFile implements Runnable {
+        private File[] files;
+
+        PickFile() {
+            files = new File(root).listFiles();
+        }
+
+        @Override
+        public void run() {
+            searchFiles(files);
+            queue.offer(ParallelSearch.STOP_WORD);
+        }
+
+        private void searchFiles(File[] listFiles) {
+            for (File f : listFiles) {
+                if (f.isDirectory()) {
+                    searchFiles(f.listFiles());
+                } else {
+                    String ext = f.getName().contains(".")? f.getName().substring(f.getName().indexOf('.') + 1) : " ";
+                    if (exts.contains(ext)) {
+                        synchronized (lock) {
+                            queue.offer(f.getAbsolutePath());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Class to analyze files for source text
+     */
+    private class Visitor implements Runnable {
+
+        @Override
+        public void run() {
+            List<String> lines;
+            Path p;
+            String file;
+            while (true) {
+                try {
+                    synchronized (lock) {
+                        if (queue.isEmpty()) {
+                            Thread.sleep(5);
+                            continue;
+                        } else {
+                            file = queue.poll();
+                        }
+                    }
+
+                    if (file.equals(ParallelSearch.STOP_WORD)) {
+                        System.out.println(result.size());
+                        break;
+                    }
+
+
+                    p = Paths.get(file);
+                    lines = Files.readAllLines(p);
+                    for (String line : lines) {
+                        if (line.contains(text)) {
+                            result.add(p.toFile().getAbsolutePath());
+                            break;
+                        }
+                    }
+                } catch (InterruptedException | IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+}
