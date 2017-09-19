ea9e83d282b5f41b1d4adaf02d3f7a37514680f0
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-09-19 10:01:13 +0300

    SimpleLock class, task #1100

---
+++ chapter_007/src/main/java/ru/job4j/wait/SimpleLock.java
@@ -0,0 +1,33 @@
+package ru.job4j.wait;
+
+/**
+ * Created on 18.09.17
+ * My simple lock realization.
+ * @author Wamdue
+ * @version 1.0
+ */
+public class SimpleLock {
+    private boolean isLocked = false;
+
+    /**
+     * Making lock.
+     */
+    public synchronized void lock() {
+        try {
+            while (isLocked) {
+                wait();
+            }
+        }catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        isLocked = true;
+    }
+
+    /**
+     * Unlocks target.
+     */
+    public synchronized void unlock() {
+        isLocked = false;
+        notify();
+    }
+}
