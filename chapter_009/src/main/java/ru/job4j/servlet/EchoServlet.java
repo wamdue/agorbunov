7e36605bea172a30007de34f6e97aa6fc93fc0b5
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-10-20 14:53:06 +0300

    First web project, task #2514

---
+++ chapter_009/src/main/java/ru/job4j/servlet/EchoServlet.java
@@ -0,0 +1,25 @@
+package ru.job4j.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created on 20.10.17
+ *
+ * @author Wamdue
+ * @version 1.0
+ */
+public class EchoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("Hello world!");
+        writer.flush();
+    }
+}
