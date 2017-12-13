6f8c807be3cd40c8d49ff391fa6c59eafb0607bc
Alexey Gorbunov <wamdue.m@yandex.ru>
2017-12-13 16:05:30 +0300

    Console chat, refactorign: add tests and move key words to constants. Task #861.

--- chapter_003_io/src/main/java/ru/job4j/io/ConsoleChat.java
+++ chapter_003_io/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -3,6 +3,8 @@ package ru.job4j.io;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -17,6 +19,19 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class ConsoleChat {
+    /**
+     * Stop word to stop answering on questions.
+     */
+    private static final String STOP = "стоп";
+    /**
+     * Word to continue answer on queastions.
+     */
+    private static final String CONTINUE = "продолжить";
+    /**
+     * Word to stop chat.
+     */
+    private static final String END = "закончить";
+
     /**
      * List of possible answers.
      */
@@ -35,7 +50,8 @@ public class ConsoleChat {
      */
     public ConsoleChat() {
         try {
-            this.answers = Files.readAllLines(Paths.get("answers.txt"));
+            File file = new File("answers.txt");
+            this.answers = Files.readAllLines(Paths.get(file.getAbsolutePath()));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,40 +60,45 @@ public class ConsoleChat {
     /**
      * Start chat method.
      */
-    public void startChat() {
-        boolean isConversation = true;
-        boolean isWorking = true;
-        Scanner scanner = new Scanner(System.in);
-        String line;
-        try (FileOutputStream out = new FileOutputStream(chatLog, true);
-             PrintStream stream = new PrintStream(out)) {
+    public void startChat(InputStream inputStream, OutputStream outputStream) {
+        if (answers.size() > 0) {
+            boolean isConversation = true;
+            boolean isWorking = true;
+            Scanner scanner = new Scanner(inputStream);
+            String line;
+            System.setOut(new PrintStream(outputStream));
+            try (FileOutputStream out = new FileOutputStream(chatLog, true);
+                 PrintStream stream = new PrintStream(out)) {
 
-            while (isWorking) {
-                line = scanner.nextLine();
-                stream.println(line);
-                if (line.equals("стоп")) {
-                    isConversation = false;
-                    continue;
-                }
+                while (isWorking) {
+                    line = scanner.nextLine();
+                    stream.println(line);
+                    if (STOP.equals(line)) {
+                        isConversation = false;
+                        continue;
+                    }
 
-                if (line.equals("продолжить")) {
-                    isConversation = true;
-                    continue;
-                }
+                    if (CONTINUE.equals(line)) {
+                        isConversation = true;
+                        continue;
+                    }
 
-                if (line.equals("закончить")) {
-                    isWorking = false;
-                    continue;
-                }
-                if (isConversation) {
-                    String answer = this.getAnswer();
-                    System.out.println(answer);
-                    stream.println(answer);
-                }
+                    if (END.equals(line)) {
+                        isWorking = false;
+                        continue;
+                    }
+                    if (isConversation) {
+                        String answer = this.getAnswer();
+                        System.out.println(answer);
+                        stream.println(answer);
+                    }
 
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("No answers in file.");
         }
     }
 
@@ -95,6 +116,6 @@ public class ConsoleChat {
      */
     public static void main(String[] args) {
         ConsoleChat chat = new ConsoleChat();
-        chat.startChat();
+        chat.startChat(System.in, System.out);
     }
 }
---
+++ chapter_003_io/src/test/java/ru/job4j/io/ConsoleChatTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.io;
+
+import com.google.common.base.Joiner;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created on 13.12.17.
+ * Console chat test.
+ * @author Wamdue
+ * @version 1.0
+ */
+public class ConsoleChatTest {
+    /**
+     * Line separator.
+     */
+    private final static String LN = System.getProperty("line.separator");
+
+    /**
+     * Exit test.
+     * When enter : закончить
+     * Expecting exit loop and program.
+     */
+    @Test
+    public void whenEnterEndWordThenExit() {
+        this.taskCase("закончить", false);
+    }
+
+    /**
+     * Stop word test.
+     * When enter : стоп
+     * Expecting no answering on other words.
+     */
+    @Test
+    public void whenEnterStopWordThenNoAnswer() {
+        this.taskCase(Joiner.on(LN).join("стоп", "not important","закончить"), false);
+    }
+
+    /**
+     * Answers test.
+     * When enter several words.
+     * Expecting answers from bot.
+     */
+    @Test
+    public void whenEnterWordThenHaveAnswer() {
+        this.taskCase(Joiner.on(LN).join("one", "two", "three", "закончить"), true);
+    }
+    /**
+     * Answers test.
+     * When word : стоп
+     * Then enter : продолжить
+     * and then enters more words.
+     * Expecting answers from bot.
+     */
+    @Test
+    public void whenEnterStopWordAndContinueThenHaveAnswer() {
+        this.taskCase(Joiner.on(LN).join("стоп", "продолжить", "three", "закончить"), true);
+    }
+
+    /**
+     * Frequency used code.
+     * @param example - example string.
+     * @param expect - expected result.
+     */
+    private void taskCase(String example, boolean expect) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(example.getBytes());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        ConsoleChat chat = new ConsoleChat();
+        chat.startChat(inputStream, outputStream);
+        assertThat(expect, is(outputStream.toString().length() > 0));
+
+    }
+
+}
