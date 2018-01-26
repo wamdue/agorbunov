0785e5d78dc52ea245ae90e1b8074d08268ff48a
Alexey Gorbunov <wamdue.m@yandex.ru>
2018-01-26 15:59:58 +0300

    Car shop, next step, task #4747 add temp values in tables

---
+++ chapter_010/src/main/resources/fill/engine.sql
@@ -0,0 +1,5 @@
+insert into engine (name) values('Бензин');
+insert into engine (name) values('Дизель');
+insert into engine (name) values('Гибрид');
+insert into engine (name) values('Электро');
+insert into engine (name) values('Газ');
---
+++ chapter_010/src/main/resources/fill/gearbox.sql
@@ -0,0 +1,4 @@
+insert into gearbox (name) values('Автомат');
+insert into gearbox (name) values('Механика');
+insert into gearbox (name) values('Робот');
+insert into gearbox (name) values('Вариатор');
