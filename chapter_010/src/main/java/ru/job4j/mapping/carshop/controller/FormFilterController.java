e59a9cca8070b8d5da1f89d5bec6e75fc87cfafb
Alexey Gorbunov <wamdue.m@yandex.ru>
2018-01-31 16:18:38 +0300

    Car shop, task 4745. small problem with filling brands

---
+++ chapter_010/src/main/java/ru/job4j/mapping/carshop/controller/FormFilterController.java
@@ -0,0 +1,39 @@
+package ru.job4j.mapping.carshop.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.job4j.mapping.carshop.entity.Car;
+import ru.job4j.mapping.carshop.model.Connect;
+import ru.job4j.mapping.carshop.model.repository.CarRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Created on 31.01.18.
+ * Filterlist of cars.
+ * @author Wamdue
+ * @version 1.0
+ */
+public class FormFilterController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String brand = req.getParameter("brand");
+        String pic = req.getParameter("pic");
+        String time = req.getParameter("time");
+
+        int brandId = Integer.valueOf(brand);
+        int p = "on".equals(pic) ? 1 : 0;
+        int t = "on".equals(time) ? 1 : 0;
+
+        CarRepository repository = new CarRepository(Connect.INSTANCE.getConnection());
+        List<Car> list = repository.getByFilter(brandId, p, t);
+        ObjectMapper mapper = new ObjectMapper();
+        OutputStream out = resp.getOutputStream();
+        mapper.writeValue(out, list);
+    }
+}
--- chapter_010/src/main/java/ru/job4j/mapping/carshop/model/repository/CarRepository.java
+++ chapter_010/src/main/java/ru/job4j/mapping/carshop/model/repository/CarRepository.java
@@ -1,8 +1,16 @@
 package ru.job4j.mapping.carshop.model.repository;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import ru.job4j.mapping.carshop.entity.Car;
 import ru.job4j.mapping.carshop.model.DB;
 import ru.job4j.mapping.carshop.model.dao.CarDao;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created on 29.01.18.
@@ -11,6 +19,10 @@ import ru.job4j.mapping.carshop.model.dao.CarDao;
  * @version 1.0
  */
 public class CarRepository extends CarDao {
+    /**
+     * Temp queue.
+     */
+    private static final String QUEUE = "from Car where 1 = 1 ";
     /**
      * Main constructor.
      *
@@ -28,6 +40,63 @@ public class CarRepository extends CarDao {
         Car car = this.getById(carId);
         car.setStatus(car.getStatus() == 0 ? 1 : 0);
         this.update(car);
+    }
+
+    /**
+     * Get list of cars by brand.
+     * @param brandId - brand id.
+     * @return list.
+     */
+    @SuppressWarnings("unchecked")
+    public List<Car> getByBrand(int brandId) {
+        Query query;
+        try (Session session = this.getDb().getSession()) {
+            query = session.createQuery("from Car where brand = :brand");
+            query.setParameter("brand", brandId);
+        }
+        return query.list();
+    }
+
+    /**
+     * Get list of cars with pics.
+     * @return list.
+     */
+    @SuppressWarnings("unchecked")
+    public List<Car> getWithPics() {
+        List<Car> list;
+        try (Session session = this.getDb().getSession()){
+            list = session.createQuery("from Car as c join c.pics").list();
+        }
+        return list.stream().filter(x-> x.getPics().size() > 0).collect(Collectors.toList());
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Car> getByFilter(int brand, int pic, int date) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(QUEUE);
+        List<Car> list;
+        try (Session session = this.getDb().getSession()) {
+            if (brand > 0) {
+                sb.append("and brand = :brand ");
+            }
+            if (date > 0) {
+                sb.append("and post >= :time");
+            }
+            Query query = session.createQuery(sb.toString());
+            if (brand > 0) {
+                query.setParameter("brand", brand);
+            }
+            if (date > 0) {
+                LocalDate local = LocalDate.now();
+                Timestamp timestamp = new Timestamp( local.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+                query.setParameter("time", timestamp);
+            }
+            list = query.list();
+            if (pic > 0) {
+                list = list.stream().filter(c -> c.getPics().size() > 0).collect(Collectors.toList());
+            }
+        }
+        return list;
     }
 
 }
